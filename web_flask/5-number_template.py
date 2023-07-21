245696f961af532a5dd07674396f1a8da5acd783
sentense1 <3xsaint@gmail.com>
2023-07-21 05:17:21 -0700

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -34,12 +34,9 @@ def python_with_text(text):
 def number(n):
     return "{} is a number".format(n)
 
-@app.route('/number_teplate/<int:n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    if isinstance(n, int):
         return render_template("5-number.html", n=n)
-    else:
-        return "invalid input. please provide an integer"
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port =5000)
