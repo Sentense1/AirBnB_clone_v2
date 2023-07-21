2c9f0889a0d38e0d0ed644d84a46fff5ff036563
sentense1 <3xsaint@gmail.com>
2023-07-21 13:30:41 -0700

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-""" """
 
-from flask import Flask, render_template
-from models import storage
-
+from flask import Flask, render_template, teardown_appcontext
+from models import storage, State
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
 
 @app.teardown_appcontext
-def teardown_app_contxt(exception):
-    """ """
+def close_storage(error):
+    """Closes the storage after each request."""
     storage.close()
 
+
+@app.route('/states_list')
+def states_list():
+    """Displays a list of all State objects present in DBStorage."""
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda s: s.name)
+
+    return render_template('7-states_list.html', states=sorted_states)
+
+
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host="0.0.0.0", port=5000)
