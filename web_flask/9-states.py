826985604bec253a0342922af6c0e05668dab5ba
sentense1 <3xsaint@gmail.com>
2023-07-22 09:41:00 -0700

    task10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -9,7 +9,9 @@ from models.city import City
 
 app = Flask(__name__)
 
-# Teardown app context to remove the current SQLAlchemy session after each request
+
+# Teardown app context to remove the current
+#   SQLAlchemy session after each request
 @app.teardown_appcontext
 def teardown_app_context(exception):
     """Remove the current SQLAlchemy session."""
@@ -23,24 +25,27 @@ def states():
 
     States are sorted by name.
     """
-    # Fetch all State objects from the DBStorage and sort them by name (A->Z)
-    states = sorted(storage.all(State).values(), key=lambda s: s.name)
+    # Fetch all State objects from the storage (FileStorage or DBStorage)
+    states = storage.all("State")
 
-    # Render the template and pass the list of states to the template
-    return render_template('states.html', states=states)
+    # Render the "9-states.html" template and pass the list of states as the variable 'state'
+    return render_template("9-states.html", state=states)
 
 
 # Define the route for '/states/<id>'
 @app.route('/states/<id>', strict_slashes=False)
 def states_by_id(id):
     """Displays an HTML page with info about <id>, if it exists."""
-    # Loop through all State objects in the storage (either FileStorage or DBStorage)
+    # Loop through all State objects in the storage
+    #   (either FileStorage or DBStorage)
     for state in storage.all("State").values():
         # Check if the current state object's id matches the requested <id>
         if state.id == id:
-            # If a matching state is found, render the template and pass the state object to the template
+            # If a matching state is found,
+            #   render the template and pass the state object to the template
             return render_template("9-states.html", state=state)
-    # If no matching state is found, render the template without passing any state object
+    # If no matching state is found,
+    #   render the template without passing any state object
     return render_template("9-states.html")
 
 
