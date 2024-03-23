2eebf90f7736af69cd39af0423e488834245d562
Johnson Oragui <johnson.oragui@gmail.com>
2024-03-23 23:14:15 +0100

    updated

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 """ Starts a Flask web application. """
-
 from flask import Flask, render_template
 from models import storage
 
 # creates an instance of the Flask class and assigns it to the variable app
 app = Flask(__name__)
+
+
+# Define the route for '/states_list'
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """
+    Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
+    """
+    # Render the template and pass the list of states to the template
+    return render_template("7-states_list.html", states=storage.all('State'))
 
 
 # Teardown app context to remove the
@@ -14,20 +24,6 @@ app = Flask(__name__)
 def teardown(exception):
     """Remove the current SQLAlchemy session."""
     storage.close()
-
-
-# Define the route for '/states_list'
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-
-    States are sorted by name.
-    """
-    # Fetch all State objects from the DBStorage and sort them by name (A->Z)
-    # states = sorted(storage.all(State).values(), key=lambda s: s.name)
-
-    # Render the template and pass the list of states to the template
-    return render_template("7-states_list.html", states=storage.all('State'))
 
 
 if __name__ == "__main__":
