a55a8548ae44aa14813ce8d0cb16985f172a795b
Johnson Oragui <johnson.oragui@gmail.com>
2024-04-22 14:00:35 +0100

    Update place.py

--- models/place.py
+++ models/place.py
@@ -11,7 +11,7 @@ from models.review import Review
 from models.amenity import Amenity
 
 # Define the association table
-place_amenity = Table('place_amenity', Base.metadata,
+association_table = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60), ForeignKey("places.id"),
                              primary_key=True, nullable=False),
                       Column('amenity_id', String(60),
