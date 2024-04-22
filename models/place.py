8b56ff6d3ed182105ed31393733726903daa9b8f
Johnson Oragui <johnson.oragui@gmail.com>
2024-04-22 13:23:41 +0100

    Update place.py

--- models/place.py
+++ models/place.py
@@ -34,18 +34,6 @@ class Place(BaseModel, Base):
     amenity_ids = []
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
-        amenity_ids = []
-
         @property
         def reviews(self):
             """Get a list of all linked Reviews."""
