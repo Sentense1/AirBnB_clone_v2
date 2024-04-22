e897490dc799b75b3e16b5aea227489d6b509ea0
Johnson Oragui <johnson.oragui@gmail.com>
2024-04-22 12:58:41 +0100

    Update db_storage.py to import place_amenity

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -9,6 +9,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from models.place_amenity import PlaceAmenity
 from sqlalchemy import create_engine
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import scoped_session
