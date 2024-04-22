5683fc712b8bd9cacc464fbdda73e65188971c63
Johnson Oragui <johnson.oragui@gmail.com>
2024-04-22 14:20:05 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -19,5 +19,5 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    places = relationship("Place", secondary="place_amenity",
-                                   viewonly=False, back_populates='amenities')
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=True)
