403558064a8d2f842d2fa0b41a7fb3892dcf3565
johnson-oragui <johnson-oragui@gmail.com>
2024-04-24 11:38:05 +0100

    fixed checker 1

--- models/place.py
+++ models/place.py
@@ -11,15 +11,15 @@ from models.review import Review
 from models.amenity import Amenity
 
 
-if getenv("HBNB_TYPE_STORAGE") and getenv("HBNB_TYPE_STORAGE") == "db" :
+if getenv("HBNB_TYPE_STORAGE") and getenv("HBNB_TYPE_STORAGE") == "db":
     # Define the association table
     place_amenity = Table('place_amenity', Base.metadata,
-                        Column('place_id', String(60), ForeignKey("places.id"),
-                                primary_key=True, nullable=False),
-                        Column('amenity_id', String(60),
-                                ForeignKey("amenities.id"),
-                                primary_key=True, nullable=False))
-
+                          Column('place_id', String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True, nullable=False))
 
     class Place(BaseModel, Base):
         """ A place to stay
@@ -36,15 +36,26 @@ if getenv("HBNB_TYPE_STORAGE") and getenv("HBNB_TYPE_STORAGE") == "db" :
         latitude = Column(Float)
         longitude = Column(Float)
         reviews = relationship("Review", backref="place",
-                            cascade="all, delete-orphan")
+                               cascade="all, delete-orphan")
         amenities = relationship("Amenity", secondary="place_amenity",
-                                viewonly=False)
-        amenity_ids = []
+                                 viewonly=False)
 else:
     class Place(BaseModel):
         """
         A place to stay
         """
+        city_id = ''
+        user_id = ''
+        name = ''
+        description = ''
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
         @property
         def reviews(self):
             """Get a list of all linked Reviews."""
