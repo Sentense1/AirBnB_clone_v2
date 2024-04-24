4b653bb2bcb01c9211fb10439de0ffa9404812c8
johnson-oragui <johnson-oragui@gmail.com>
2024-04-24 11:11:21 +0100

    modified to create Place class without declarative_base for filestorage

--- models/place.py
+++ models/place.py
@@ -11,36 +11,51 @@ from models.review import Review
 from models.amenity import Amenity
 
 
-# Define the association table
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60), ForeignKey("places.id"),
-                             primary_key=True, nullable=False),
-                      Column('amenity_id', String(60),
-                             ForeignKey("amenities.id"),
-                             primary_key=True, nullable=False))
+if getenv("HBNB_TYPE_STORAGE") and getenv("HBNB_TYPE_STORAGE") == "db" :
+    # Define the association table
+    place_amenity = Table('place_amenity', Base.metadata,
+                        Column('place_id', String(60), ForeignKey("places.id"),
+                                primary_key=True, nullable=False),
+                        Column('amenity_id', String(60),
+                                ForeignKey("amenities.id"),
+                                primary_key=True, nullable=False))
 
 
-class Place(BaseModel, Base):
-    """ A place to stay
-    """
-    __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, default=0)
-    number_bathrooms = Column(Integer, default=0)
-    max_guest = Column(Integer, default=0)
-    price_by_night = Column(Integer, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    reviews = relationship("Review", backref="place",
-                           cascade="all, delete-orphan")
-    amenities = relationship("Amenity", secondary="place_amenity",
-                             viewonly=False)
-    amenity_ids = []
-
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+    class Place(BaseModel, Base):
+        """ A place to stay
+        """
+        __tablename__ = "places"
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024))
+        number_rooms = Column(Integer, default=0)
+        number_bathrooms = Column(Integer, default=0)
+        max_guest = Column(Integer, default=0)
+        price_by_night = Column(Integer, default=0)
+        latitude = Column(Float)
+        longitude = Column(Float)
+        reviews = relationship("Review", backref="place",
+                            cascade="all, delete-orphan")
+        amenities = relationship("Amenity", secondary="place_amenity",
+                                viewonly=False)
+        amenity_ids = []
+else:
+    class Place(BaseModel):
+        """
+        A place to stay
+        """
+        city_id = ''
+        user_id = ''
+        name = ''
+        description = ''
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.00
+        longitude = 0.00
+        amenity_ids = []
         @property
         def reviews(self):
             """Get a list of all linked Reviews."""
