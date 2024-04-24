7afe564476a0092d240f5d08b79038b7d7b67689
johnson-oragui <johnson-oragui@gmail.com>
2024-04-24 11:32:21 +0100

    trying another fix

--- models/place.py
+++ models/place.py
@@ -44,16 +44,6 @@ else:
         """
         A place to stay
         """
-        city_id = ''
-        user_id = ''
-        name = ''
-        description = ''
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.00
-        longitude = 0.00
         amenity_ids = []
         @property
         def reviews(self):
