b86c8b61ab2bbd9c4f645f5259dc7df75c4a1854
johnson-oragui <johnson-oragui@gmail.com>
2024-04-25 13:19:55 +0100

    moved __table__

--- models/place.py
+++ models/place.py
@@ -28,25 +28,24 @@ if STORAGE_TYPE and STORAGE_TYPE == "db":
 class Place(BaseModel, Base):  # type: ignore
     """ A place to stay
     """
+    __tablename__ = "places"
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, default=0)
+    number_bathrooms = Column(Integer, default=0)
+    max_guest = Column(Integer, default=0)
+    price_by_night = Column(Integer, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
     if STORAGE_TYPE == 'db':
-        __tablename__ = "places"
-        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024))
-        number_rooms = Column(Integer, default=0)
-        number_bathrooms = Column(Integer, default=0)
-        max_guest = Column(Integer, default=0)
-        price_by_night = Column(Integer, default=0)
-        latitude = Column(Float)
-        longitude = Column(Float)
         reviews = relationship("Review", backref="place",
                                cascade="all, delete-orphan")
         amenities = relationship("Amenity", secondary="place_amenity",
                                  viewonly=False,
                                  cascade="all, delete")
     else:
-        __tablename__ = "places"
         city_id = ''
         user_id = ''
         name = ''
