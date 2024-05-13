ccbbd95c789c77d333e6cb22f8b6e39e633bf791
johnson-oragui <johnson-oragui@gmail.com>
2024-05-13 15:06:32 +0100

    modified

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
 """Starts a Flask web application.
 """
-
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
     """ Displays 'Hello HBNB!'. """
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
     """Displays 'HBNB'."""
     return "HBNB"
 
 
 if __name__ == "__main__":
-    # Start the Flask development server
-    # Listen on all available network interfaces (0.0.0.0) and port 5000
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
