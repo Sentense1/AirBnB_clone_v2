c929613546332862288d5100b5c202a588d02198
johnson-oragui <johnson-oragui@gmail.com>
2024-05-16 21:27:32 +0100

    mofified

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,18 +3,9 @@
 from flask import Flask, render_template
 from models import storage
 
-
 # creates an instance of the Flask class and assigns
 # it to the variable app
 app = Flask(__name__)
-
-
-# Teardown app context to remove the
-#   current SQLAlchemy session after each request
-@app.teardown_appcontext
-def teardown(exception):
-    """Remove the current SQLAlchemy session."""
-    storage.close()
 
 
 # Define the route for '/states_list'
@@ -25,5 +16,11 @@ def states_list():
     return render_template('7-states_list.html', states=states)
 
 
+@app.teardown_appcontext
+def teardown(exception):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
