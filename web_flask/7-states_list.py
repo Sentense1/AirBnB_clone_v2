f20a478547be3528c58615f8a6dfdee3101e4331
johnson-oragui <johnson-oragui@gmail.com>
2024-05-16 15:37:02 +0100

    modified

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,9 +15,12 @@ def states_list():
     Displays an HTML page with a list of all State objects in DBStorage.
     States are sorted by name.
     """
+    states_dict = storage.all('State').values()
+    states = [obj.to_dict() for obj in states_dict]
+    sorted_states = sorted(states, key=lambda state: state.get('name'))
     # Render the template and pass the list of states to the template
     return render_template("7-states_list.html",
-                           states=storage.all('State'))
+                           states=sorted_states)
 
 
 # Teardown app context to remove the
@@ -31,4 +34,4 @@ def teardown(exception):
 if __name__ == "__main__":
     # Start the Flask development server
     # Listen on all available network interfaces (0.0.0.0) and port 5000
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
