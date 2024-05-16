42b88ee2a7516607a00c49f1ae88de50adf63f03
johnson-oragui <johnson-oragui@gmail.com>
2024-05-16 20:59:49 +0100

    mofified

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,13 @@
 #!/usr/bin/python3
 """ Starts a Flask web application. """
 from flask import Flask, render_template
-from models import storage
 from models.state import State
+from models import storage
+
 
 # creates an instance of the Flask class and assigns
 # it to the variable app
 app = Flask(__name__)
-
-
-# Define the route for '/states_list'
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """
-    Displays an HTML page with a list of all State objects in DBStorage.
-    States are sorted by name.
-    """
-    # states_dict = storage.all('State').values()
-    # states = [obj.to_dict() for obj in states_dict]
-    # sorted_states = sorted(states, key=lambda state: state.get('name'))
-    # Render the template and pass the list of states to the template
-    return render_template("7-states_list.html",
-                           states=storage.all(State))
 
 
 # Teardown app context to remove the
@@ -32,7 +18,14 @@ def teardown(exception):
     storage.close()
 
 
+# Define the route for '/states_list'
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ displays a HTML page with a list of states """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    return render_template('7-states_list.html', states=states)
+
+
 if __name__ == "__main__":
-    # Start the Flask development server
-    # Listen on all available network interfaces (0.0.0.0) and port 5000
     app.run(debug=True, host="0.0.0.0", port=5000)
