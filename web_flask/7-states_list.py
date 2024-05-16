d51ea97d575218d54f11edbf78063703e566d079
johnson-oragui <johnson-oragui@gmail.com>
2024-05-16 21:09:02 +0100

    mofified

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -21,7 +21,7 @@ def teardown(exception):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ displays a HTML page with a list of states """
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    states = storage.all('State')
     return render_template('7-states_list.html', states=states)
 
 
